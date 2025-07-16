be02d61c29a32c56a89b3096861b6a8e72f0fd8f
Tessyrich <olyrich3@gmail.com>
2025-07-16 22:32:16 +0100

    update website

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem "solid_cable"
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+gem 'rest-client'
 
 # Deploy this application anywhere as a Docker container [https://kamal-deploy.org]
 gem "kamal", require: false
---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,62 @@
+class KittensController < ApplicationController
+  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+def index
+  @kittens = Kitten.all
+
+  respond_to do |format|
+    format.html
+    format.json { render json: @kittens }
+  end
+end
+
+def show
+  respond_to do |format|
+    format.html
+    format.json { render json: @kitten }
+  end
+end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:notice] = "Kitten added successfully!"
+      redirect_to @kitten
+    else
+      flash.now[:alert] = "Error adding kitten!"
+      render :new
+    end
+  end
+
+  def update
+    if @kitten.update(kitten_params)
+      flash[:notice] = "Kitten updated successfully!"
+      redirect_to @kitten
+    else
+      flash.now[:alert] = "Error updating kitten!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten.destroy
+    flash[:notice] = "Kitten deleted!"
+    redirect_to kittens_path
+  end
+
+  private
+
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
